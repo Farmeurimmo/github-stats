eb12107ef18356b1d190c2a139a0447c4a957a58
Farmeurimmo <contact@farmeurimmo.fr>
2025-06-03 21:58:56 +0200

    Update github_stats.py

--- github_stats.py
+++ github_stats.py
@@ -73,7 +73,7 @@ class Queries(object):
         :return: deserialized REST JSON output
         """
 
-        for _ in range(3):
+        for _ in range(6):
             headers = {
                 "Authorization": f"token {self.access_token}",
             }
